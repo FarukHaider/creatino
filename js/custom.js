5a85c9333aa43064446234ba4c5d185790c3d78b
pranon <cto@pranon.com>
2022-10-30 14:27:40 +0600

    complete without about image

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+
+(function ($) {
+	'use strict';
+
+	$(document).ready(function () {
+		ma5menu({
+			menu: '.navbar-nav	',
+			activeClass: 'current',
+			position: 'right',
+			closeOnBodyClick: true
+		});
+	});
+
+	
+	
+
+	
+
+})(jQuery);
+	
